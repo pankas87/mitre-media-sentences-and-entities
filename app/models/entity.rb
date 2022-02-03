e1ef4a0572089aa0d5fdcdd9224cd5eee911f19d
Leonardo Graterol <pankas87@gmail.com>
2022-02-03 19:00:07 -0300

    Entity model, implementation

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,9 @@
 class Entity < ApplicationRecord
+  # Validations
+  validates :text, presence: true
+  validates :type, presence: true
+  validates :sentence, presence: true
+
+  # Associations
   belongs_to :sentence
 end
