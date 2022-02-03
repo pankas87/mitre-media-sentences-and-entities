7ad6029c29f2cce0003293c8ff235c785a56e7e7
Leonardo Graterol <pankas87@gmail.com>
2022-02-03 18:50:42 -0300

    Sentence model specs

--- spec/models/sentence_spec.rb
+++ spec/models/sentence_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Sentence, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Attributes Validations
+  it { should validate_presence_of(:text) }
+
+  # Associations
+  it { should have_many(:entities) }
+
+  # Behavior
+  describe "identifying entities inside a sentence" do
+    pending "Implement when it corresponds"
+  end
 end
