00ec59d626b093fabe46792d564dd7f1f7abaa03
Leonardo Graterol <pankas87@gmail.com>
2022-02-03 19:16:53 -0300

    Sentences e2e spec, skeleton

---
+++ spec/system/view_sentences_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+Rspec.describe "ViewSentences", :type => :system do
+  describe "List of Sentences" do
+    it "shows a formatted list of sentences" do
+      pending "Assert: The list of sentences is correct"
+      pending "Assert: The format is correct"
+    end
+  end
+
+  describe "Show one Sentence" do
+    it "shows the requested sentence" do
+      pending "Assert: The displayed sentences is correct"
+      pending "Assert: The format is correct"
+    end
+  end
+end
