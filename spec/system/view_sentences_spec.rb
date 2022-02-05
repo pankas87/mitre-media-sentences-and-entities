f1147873dc88d7cbe7c36e4a963489de69c22f9c
Leonardo Graterol <pankas87@gmail.com>
2022-02-05 00:42:13 -0300

    Sentences, controllers and view, specs skeleton (WIP, Webpack failing)

--- spec/system/view_sentences_spec.rb
+++ spec/system/view_sentences_spec.rb
@@ -5,8 +5,26 @@ RSpec.describe "ViewSentences", :type => :system do
     it "shows a formatted list of sentences" do
       visit "/sentences"
 
-      pending "Assert: The list of sentences is correct"
-      pending "Assert: The format is correct"
+      expect(page).to have_content 'Sentences'
+
+      # TODO: Expect that there is an HTML <ol> with three <li>
+      # TODO: Expect that for every sentence in the DB, there is one corresponding <li> with the correct format
+      #         - Create the formatted_text in both the Entity and Sentence models
+      #           - Format by adding <span> element for entities
+
+      sentences = Sentence.all
+      expect(sentences.length > 0).to be true
+
+      sentences.each_with_index do |sentence, i|
+        # TODO: With this one just validate that the sentences are print, without using the custom text formatting method
+        element_selector = "#sentence-1"
+        element = find(element_selector)
+
+        expect(element).not_to be_nil
+        expect(element.text).to eq(sentence.text)
+        # TODO: Implement the text formatting nested methos and test (Probably change the expected text from the previous text)
+        pending "The format of the sentence entities text must be correct"
+      end
     end
   end
 
