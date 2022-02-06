355f0b2bf215fe23af6f9d31305b9fa763060208
Leonardo Graterol <pankas87@gmail.com>
2022-02-06 20:55:56 -0300

    EntitiesController#create, spec and implementation

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,5 +1,17 @@
 class EntitiesController < ApplicationController
   def create
+    @sentence = Sentence.find(params[:sentence_id])
 
+    @entity = Entity.new(sentence_params)
+    @entity.sentence = @sentence
+    # TODO: Set flash error message for the frontend (If time is available)
+    @entity.save
+    redirect_to "/sentences/#{@sentence.id}"
+  end
+
+  private
+
+  def sentence_params
+    params.permit(:text, :type_of)
   end
 end
--- spec/request/entities_controller_spec.rb
+++ spec/request/entities_controller_spec.rb
@@ -1,9 +1,41 @@
-require 'rspec'
+require 'rails_helper'
 
 # Look at the other request specs, the ones in the ETH NFT Ethereum
 describe 'EntitiesController', type: :request do
   describe "#create_entity" do
-    pending "creates a new entity with valid parameters and redirects back to the sentence page"
-    pending "redirects back to the sentence page, setting some error messages"
+    context "valid parameters" do
+      it "creates a new entity with valid parameters and redirects back to the sentence page" do
+        sentence = Sentence.first
+        text = sentence.available_phrases[0]
+        params = {text: text, type_of: "Financial Market"}
+        post "/sentences/#{sentence.id}/entity", params: params
+
+        # HTTP Response assertions
+        expect(response).to redirect_to("/sentences/#{sentence.id}")
+
+        # Side effects assertions
+        entity = Entity.last
+
+        expect(entity.text).to eq(params[:text])
+        expect(entity.type_of).to eq(params[:type_of])
+      end
+    end
+
+    context "invalid parameters" do
+      it "does not create a new entity and redirects back to the sentence page" do
+        prev_total_entities = Entity.count
+        sentence = Sentence.first
+        text = sentence.entities[0].text
+        params = {text: text, type_of: "Financial Market"}
+        post "/sentences/#{sentence.id}/entity", params: params
+
+        # HTTP Response assertions
+        expect(response).to redirect_to("/sentences/#{sentence.id}")
+
+        # Side effects assertions
+        expect(Entity.count).to eq(prev_total_entities)
+      end
+    end
+
   end
 end
