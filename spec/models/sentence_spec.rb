a54d45ffa9e1a7049497db72f5cd8918cf7d4471
Leonardo Graterol <pankas87@gmail.com>
2022-02-07 04:47:28 -0300

    (WIP)

--- spec/models/sentence_spec.rb
+++ spec/models/sentence_spec.rb
@@ -33,7 +33,6 @@ RSpec.describe Sentence, type: :model do
           Entity.new(text: "consectetur", type_of: "ASD"),
         ]
         sentence.save
-        # TODO: Optional, remove punctuation
         expected_phrases = [
           "Lorem ipsum",
           "amet,",
@@ -51,7 +50,7 @@ RSpec.describe Sentence, type: :model do
         expect(sentence.available_phrases).to eq(expected_phrases)
       end
 
-      # TODO: Make text multiline with paragraph and shit (If time is available)
+      # TODO: Make text multiline with paragraph (If time is available)
       pending "case 3: Tax growth 401k volatile yield management holder. Private IRA junk bonds corporate corporate bonds funds exchange investment grade bonds managed comodity Fitch bull stocks. Fitch upswing notes financial health established taxpayer. Managed quarterly debt term Moody's industry Standard & Poor's corporate. Nikkei Fitch volatile. "
     end
   end
