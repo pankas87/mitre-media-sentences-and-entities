557fa23d57a9266e9928d8d887e3de7a1638d4b2
Leonardo Graterol <pankas87@gmail.com>
2022-02-07 02:13:15 -0300

    Adding entity text dynamically to the form with Javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,25 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("DOMContentLoaded", function() {
+    var addTextButtons = document.getElementsByClassName("add-text-button")
+    function addText(event) {
+        // Getting the text that shall be added
+        var element = event.currentTarget;
+        var parent = element.parentNode;
+        var textToAdd = parent.getElementsByClassName("text-to-add")[0].innerText;
+
+        // Adding the text
+        var textInput = document.getElementById("text");
+        var currentValue = textInput.value;
+        textInput.value = (currentValue == "") ? textToAdd : currentValue + " " + textToAdd;
+        console.log(textToAdd.textContent);
+    }
+
+    for (var i = 0; i < addTextButtons.length; i++) {
+        addTextButtons[i].addEventListener('click', addText, false);
+    }
+});
+
+
